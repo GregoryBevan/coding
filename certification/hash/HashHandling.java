68225af1f306d0c51f9bb1a83ddfef863c6239c7
gbe <gbe@vif.fr>
2016-09-01 15:24:01 +0200

    remove class files

---
+++ certification/hash/HashHandling.java
@@ -0,0 +1,7 @@
+public class HashHandling {
+
+  public static void main(String... args) {
+    System.out.println("Long max : " + Long.MAX_VALUE );
+      System.out.println("décalage de bits : " + (Long.MAX_VALUE >>> 32));
+  }
+}
